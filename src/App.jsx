a2464e3e087b66a5a61379bf0b28c8069d389c07
AMMAR <141614147+AmmarTheDeveloper@users.noreply.github.com>
2024-11-19 20:20:05 +0530

    updated import of header because  it was lowercase in deployment it was giving issue because the actual file was in capital so i fixed it

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import AddText from "./components/AddText";
 import Customization from "./components/Customization";
 import EditorCanvas from "./components/EditorCanvas";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 function App() {
   return (
